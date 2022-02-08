39834b4fadc155ec270f9f54c48d14d503ff1d34
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-08 12:12:42 +0100

    Add: HeaderBox component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-
+    <header-box />
   </div>
 </template>
 
 <script>
-
+import HeaderBox from './components/HeaderBox.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderBox
   }
 }
 </script>
