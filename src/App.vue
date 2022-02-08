cc90e5d3a51e5a02224f4377c593a3c7c58f3eaf
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-08 13:19:41 +0100

    Add: HeaderContent in date

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@ export default {
   name: 'App',
   components: {
     HeaderBox
+  },
+  data(){
+    return{
+      headerContent:[
+        {
+          id:'HC01',
+          image:'../public/images/course-9-f-img.jpg',
+          title:'Contemporary Ideas',
+          text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci officia voluptas consequuntur omnis! Numquam animi est necessitatibus accusantium cupiditate expedita voluptates tenetur, magnam ut, excepturi repudiandae, iusto eos qui provident?'
+        }
+      ]
+    }
   }
 }
 </script>
